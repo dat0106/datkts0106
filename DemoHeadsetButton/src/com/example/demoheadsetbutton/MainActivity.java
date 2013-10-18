bb4535a86e2f5a3c133780f1c716c1f72bb9a8be
datkts0106@gmail.com <datkts0106@gmail.com@72380ea6-93d0-6362-9c75-ea5fc79de657>
2013-10-18 10:19:29 +0000

    git-svn-id: https://android-ledat.googlecode.com/svn/trunk@11 72380ea6-93d0-6362-9c75-ea5fc79de657

--- DemoHeadsetButton/src/com/example/demoheadsetbutton/MainActivity.java
+++ DemoHeadsetButton/src/com/example/demoheadsetbutton/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.demoheadsetbutton;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IntentFilter filter  = new IntentFilter(Intent.ACTION_MEDIA_BUTTON);
+        MediaButtonIntentReceiver r  = new MediaButtonIntentReceiver();
+        filter.setPriority(10000);
+        registerReceiver(r, filter);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
