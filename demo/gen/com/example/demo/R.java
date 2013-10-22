8b32bfa939e9e21b798fa7c13c8396166559a07b
datkts0106@gmail.com <datkts0106@gmail.com@72380ea6-93d0-6362-9c75-ea5fc79de657>
2013-10-22 03:25:19 +0000

    git-svn-id: https://android-ledat.googlecode.com/svn/trunk@13 72380ea6-93d0-6362-9c75-ea5fc79de657

--- demo/gen/com/example/demo/R.java
+++ demo/gen/com/example/demo/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int Call=0x7f080004;
+        public static final int action_settings=0x7f080005;
         public static final int log=0x7f080002;
         public static final int submit=0x7f080003;
         public static final int textView1=0x7f080000;
@@ -39,6 +40,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int call=0x7f050004;
         public static final int hello_world=0x7f050002;
         public static final int wifi=0x7f050003;
     }
--- demo/src/com/example/demo/DemoActivity.java
+++ demo/src/com/example/demo/DemoActivity.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import java.util.List;
+
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -34,33 +40,49 @@ public class DemoActivity extends Activity {
 
        submit.setOnClickListener(new OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                mainWifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+                // TODO Auto-generated method stub
+                //
+                if(wifiStatus.isChecked() == true){
+                      mainWifi.setWifiEnabled(true);
+                 }
+                  else{
+                      mainWifi.setWifiEnabled(false);
+                  }
+                  if (mainWifi.isWifiEnabled() == false)
+                  {
+                      // If wifi disabled then enable it
+                      Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+                  }
+                  else
+                  {
+                       // If wifi disabled then enable it
+                      Toast.makeText(getApplicationContext(), "wifi is enabled", Toast.LENGTH_LONG).show();
+                  }
+
+           }
+        });
+
+       call.setOnClickListener(new OnClickListener() {
+
         @Override
         public void onClick(View v) {
-            mainWifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
             // TODO Auto-generated method stub
-            //
-            if(wifiStatus.isChecked() == true){
-                  mainWifi.setWifiEnabled(true);
-             }
-              else{
-                  mainWifi.setWifiEnabled(false);
-              }
-              if (mainWifi.isWifiEnabled() == false)
-              {
-                  // If wifi disabled then enable it
-                  Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
-              }
-              else
-              {
-                   // If wifi disabled then enable it
-                  Toast.makeText(getApplicationContext(), "wifi is enabled", Toast.LENGTH_LONG).show();
-              }
+            Uri number = Uri.parse("tel:5551234");
+            Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
 
-       }
+            PackageManager packageManager = getPackageManager();
+            List<ResolveInfo> activities =  packageManager.queryIntentActivities(callIntent, 0);
+            boolean isIntentSafe = activities.size() > 0;
+            if (isIntentSafe) {
+                startActivity(callIntent);
+            }
+        }
     });
 
-
-        };
+    };
 
 
     @Override
