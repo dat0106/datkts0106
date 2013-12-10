a89e5872379d55036ef408dccee39a2b852b6db2
datkts0106@gmail.com <datkts0106@gmail.com@72380ea6-93d0-6362-9c75-ea5fc79de657>
2013-12-10 01:22:41 +0000

    git-svn-id: https://android-ledat.googlecode.com/svn/trunk@81 72380ea6-93d0-6362-9c75-ea5fc79de657

---
+++ vogella-master/com.vogella.android.listview.contextualactionbar/gen/de/vogella/android/listactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.vogella.android.listactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ vogella-master/com.vogella.android.listview.withanimations/gen/com/vogella/android/listview/withanimation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vogella.android.listview.withanimation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
