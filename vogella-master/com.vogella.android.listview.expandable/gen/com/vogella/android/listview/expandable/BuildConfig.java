84556cc78e903a228c3a235e194aa25d30083997
dat0106 <datkts0106@gmail.com>
2014-01-14 16:31:32 +0700

    Revert "fuck you"
    This reverts commit 93fe7aa6828c94bbbe2948afe9f8f13736966f49.

--- vogella-master/com.vogella.android.listview.expandable/gen/com/vogella/android/listview/expandable/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.vogella.android.listview.expandable;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
