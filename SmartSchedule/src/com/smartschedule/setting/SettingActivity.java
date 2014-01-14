7ce62d229a8ac1b0fcadd55ec960f811ace14e79
dat0106 <datkts0106@gmail.com>
2014-01-14 16:31:32 +0700

    dcm
    This reverts commit 93fe7aa6828c94bbbe2948afe9f8f13736966f49.

---
+++ SmartSchedule/src/com/smartschedule/setting/SettingActivity.java
@@ -0,0 +1,153 @@
+package com.smartschedule.setting;
+
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.smartschedule.DrawAction;
+import com.smartschedule.EventDetailActivity;
+import com.smartschedule.database.Action;
+import com.smartschedule.database.Event;
+import com.smartschedule.database.SmartSchedulerDatabase;
+import com.smartschedule.util.Constant;
+
+import android.app.ExpandableListActivity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.Toast;
+
+public class SettingActivity extends ExpandableListActivity implements
+        OnChildClickListener {
+
+    private SmartSchedulerDatabase smartScheduleDb = new SmartSchedulerDatabase(
+            this);
+    private int event_id;
+    private ContentValues eventDetail;
+    private ContentValues actionStart;
+    private ContentValues actionEnd;
+    SettingAdapter mNewAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+
+        String jString = "{\"ringtone_alarm\": \"primal\", \"rimgtome_ringer\": \"Hi, I am Primal\"}  ";
+
+        GsonBuilder gsonb = new GsonBuilder();
+        Gson gson = gsonb.create();
+        DrawAction pst = null;
+
+        pst = gson.fromJson(jString, DrawAction.class);
+//
+//        Intent cv = new Intent();
+//
+//        cv.putExtra("fuck", pst);
+//
+//        DrawAction pst1 = cv.getExtras().getParcelable("fuck");
+//
+//        Log.d("TAggeD", pst1.ringtone_alarm + pst1.rimgtome_ringer);
+//
+//        Log.d("TAGGED", gson.toJson(pst1));
+
+        this.event_id = intent.getExtras().getInt(
+                SmartSchedulerDatabase.COLUMN_EVENT_ID);
+
+        ExpandableListView expandbleLis = getExpandableListView();
+        expandbleLis.setDividerHeight(2);
+        expandbleLis.setGroupIndicator(null);
+        expandbleLis.setClickable(true);
+
+        setGroupData();
+        setChildGroupData();
+
+        mNewAdapter = new SettingAdapter(groupItem,
+                childItem, event_id);
+        mNewAdapter
+                .setInflater(
+                        (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE),
+                        this);
+        expandbleLis.setAdapter(mNewAdapter);
+        expandbleLis.setDivider(null);
+        expandbleLis.setDividerHeight(0);
+
+        for (int i = 0; i < groupItem.size(); i++) {
+            expandbleLis.expandGroup(i);
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setGroupData();
+        setChildGroupData();
+        mNewAdapter.UpdateDataChange(groupItem, childItem, event_id);
+        mNewAdapter.notifyDataSetChanged();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // when the user click start schedule
+        case android.R.id.home:
+            // Navigate "up" the demo structure to the launchpad activity.
+            // for more.
+            NavUtils.navigateUpTo(this, new Intent(this, EventDetailActivity.class));
+
+            return true;
+
+        }
+        return false;
+
+    }
+
+    public void setGroupData() {
+        // TODO String chuyen da ngon ngu
+        groupItem.clear();
+        groupItem.add("APPLICATIONs");
+        groupItem.add("SOUND");
+        groupItem.add("COMMUNICATION");
+    }
+
+    ArrayList<String> groupItem = new ArrayList<String>();
+    ArrayList<Object> childItem = new ArrayList<Object>();
+
+    public void setChildGroupData() {
+        /**
+         * Add Data For TecthNology
+         */
+        childItem.clear();
+        ArrayList<Event> child = new ArrayList<Event>();
+        smartScheduleDb.openRead();
+        Event cv = smartScheduleDb.getData(event_id);
+
+        child.add(cv);
+        childItem.add(child);
+
+        /**
+         * Add Data For start
+         */
+        ArrayList<Action> childAction = new ArrayList<Action>();
+        childAction = smartScheduleDb.getDataAction(event_id, Constant.START);
+        childItem.add(childAction);
+        /**
+         * Add Data For end
+         */
+        childAction = smartScheduleDb.getDataAction(event_id, Constant.END);
+        childItem.add(childAction);
+
+        smartScheduleDb.close();
+    }
+
+}
---
+++ SmartSchedule/src/com/smartschedule/setting/SettingAdapter.java
@@ -0,0 +1,199 @@
+package com.smartschedule.setting;
+
+import java.util.ArrayList;
+
+import com.smartschedule.EventTimeActivity;
+import com.smartschedule.R;
+import com.smartschedule.database.Action;
+import com.smartschedule.database.Event;
+import com.smartschedule.database.SmartSchedulerDatabase;
+import com.smartschedule.util.Constant;
+import com.smartschedule.util.DetailActionViewer;
+import com.smartschedule.util.Router;
+import com.smartschedule.util.Util;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SettingAdapter extends BaseExpandableListAdapter {
+
+    public ArrayList<String> groupItem;
+    public ArrayList<Object> Childtem = new ArrayList<Object>();
+    public LayoutInflater minflater;
+    public Activity activity;
+    public int event_id;
+
+    public SettingAdapter(ArrayList<String> grList, ArrayList<Object> childItem, int event_id) {
+        groupItem = grList;
+        this.Childtem = childItem;
+        this.event_id = event_id;
+    }
+
+    public void UpdateDataChange(ArrayList<String> grList, ArrayList<Object> childItem, int event_id) {
+        groupItem = grList;
+        this.Childtem = childItem;
+        this.event_id = event_id;
+    }
+
+    public void setInflater(LayoutInflater mInflater, Activity act) {
+        this.minflater = mInflater;
+        activity = act;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+            boolean isLastChild, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = minflater.inflate(R.layout.setting_child, null);
+        }
+        TextView text = (TextView) convertView.findViewById(R.id.childNameSetting);
+        TextView textDetail = (TextView) convertView.findViewById(R.id.childDetailSetting);
+        if(groupPosition == 0){
+            ArrayList<Event> tempChild = (ArrayList<Event>) Childtem.get(groupPosition);
+
+            final Event Child =  tempChild.get(childPosition);
+
+            text.setText(Child.getName());
+
+            String timer = Util
+                    .getTime(Child.getTimeStartHour())
+                    + ":"
+                    + Util.getTime(Child.getTimeStartMinute())
+                    + " ~ "
+                    + Util.getTime(Child.getTimeEndHour())
+                    + ":"
+                    + Util.getTime(Child.getTimeEndMinute());
+            textDetail.setText(timer);
+            convertView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(activity, EventTimeActivity.class);
+                    intent.putExtra(SmartSchedulerDatabase.COLUMN_EVENT_ID, event_id);
+                    activity.startActivity(intent);
+                }
+            });
+        }else{
+            ArrayList<Action> tempChild = (ArrayList<Action>) Childtem.get(groupPosition);
+
+
+            final Action Child =  tempChild.get(childPosition);
+            DetailActionViewer router = Router.routerUri.get(Child.getState());
+            text.setText(activity.getString(router.name));
+
+            textDetail.setText(Child.getStatus());
+            convertView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Router.routerActivity(event_id, Child, activity);
+
+                    Toast.makeText(activity, Child.getName(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return ((ArrayList<String>) Childtem.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return groupItem.size();
+    }
+
+    @Override
+    public void onGroupCollapsed(int groupPosition) {
+        super.onGroupCollapsed(groupPosition);
+    }
+
+    @Override
+    public void onGroupExpanded(int groupPosition) {
+        super.onGroupExpanded(groupPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+            View convertView, ViewGroup parent) {
+
+        final int StartOrEnd =  groupPosition;
+        if (convertView == null) {
+            convertView = minflater.inflate(R.layout.setting_group, null);
+        }
+        ((TextView) convertView.findViewById(R.id.group_setting)).setText(groupItem.get(groupPosition));
+        ((Button) convertView.findViewById(R.id.add_action)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO bat vao trang  lay lay setting
+                SmartSchedulerDatabase smartScheduleDb = new SmartSchedulerDatabase(
+                        activity.getBaseContext());
+                ContentValues contentValues = new ContentValues();
+                if(StartOrEnd == 1){
+                    contentValues.put(SmartSchedulerDatabase.COLUMN_ACTION_START_ID, event_id);
+                }else {
+                    contentValues.put(SmartSchedulerDatabase.COLUMN_ACTION_END_ID, event_id);
+                }
+                contentValues.put(SmartSchedulerDatabase.COLUMN_ACTION_STATE, 1);
+                contentValues.put(SmartSchedulerDatabase.COLUMN_ACTION_NAME, "demo");
+                contentValues.put(SmartSchedulerDatabase.COLUMN_ACTION_DRAW, "{}");
+                contentValues.put(SmartSchedulerDatabase.COLUMN_ACTION_STATUS, "demoStatus");
+                smartScheduleDb.open();
+                smartScheduleDb.insert_action(contentValues);
+                smartScheduleDb.close();
+                Toast.makeText(activity, "add action", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        if(groupPosition == 0) {
+            ((Button) convertView.findViewById(R.id.add_action)).setVisibility(View.INVISIBLE);
+        }
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+
+}
