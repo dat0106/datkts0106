7338487d71afbf661d093ec7dad4e974af25a5c4
ledat <datkts0106@gmail.com>
2014-08-18 16:53:55 +0700

    tiep tuc nao

--- child/Cameraservice/src/com/samples/camera/ui/UserSettingActivity.java
+++ child/Cameraservice/src/com/samples/camera/ui/UserSettingActivity.java
@@ -1,18 +1,20 @@
 package com.samples.camera.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 import com.varma.samples.camera.R;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class UserSettingActivity extends PreferenceActivity {
+public class UserSettingActivity extends PreferenceActivity implements
+        OnSharedPreferenceChangeListener  {
     private  Camera cameraDevice;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,38 +27,124 @@ public class UserSettingActivity extends PreferenceActivity {
         pref.setSummary("12");
 
 
-        ListPreference camera = (ListPreference) this.findPreference("camera");
-        Log.v(this.toString(), "camera.getEntry()" + (String)camera.getEntry());
-        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
-        CharSequence[] cameraArray =  camera.getEntries();
-        CharSequence[] cameraArrayValues =  camera.getEntryValues();
-        if(Camera.getNumberOfCameras() == 2){
-            CharSequence[] entriesCamera = new CharSequence[]{cameraArray[0], cameraArray[1]};
-            CharSequence[] entryValuesCamera = new CharSequence[]{cameraArrayValues[0], cameraArrayValues[1]};
-            camera.setEntries(entriesCamera);
-            camera.setEntryValues(entryValuesCamera);
-        }else if(Camera.getNumberOfCameras() == 1){
-            CharSequence[] entriesCamera = new CharSequence[]{cameraArray[0]};
-            CharSequence[] entryValuesCamera = new CharSequence[]{cameraArrayValues[0]};
-            camera.setEntries(entriesCamera);
-            camera.setEntryValues(entryValuesCamera);
-        }else{
-            new Throwable("No camera");
-        }
+        final ListPreference cameraPreference = (ListPreference) this.findPreference("camera");
 
-        Log.v(this.toString(), "camera.getEntry()" + (String)camera.getEntry());
-//
-//        Camera.Parameters paramsCamera = cameraDevice.getParameters();
+        setCameraPreferences(cameraPreference);
+
+        final ListPreference videoQualityPreference = (ListPreference) this.findPreference("video_quality");
+        setSizePreferences(videoQualityPreference, cameraPreference);
+
+        cameraPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                cameraPreference.setSummary(cameraPreference.getEntry());
+                setSizePreferences(videoQualityPreference, cameraPreference);
+                return true;
+            }
+        });
+
+        Log.v(this.toString(), "camera.getEntry()" + (String)cameraPreference.getEntry());
+
 
         pref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 Intent intent =  new Intent(getApplicationContext(), MyVideo.class);
                 startActivity(intent);
-                return false;
+
+                return true;
             }
         });
 	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(UserSettingActivity.this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener( UserSettingActivity.this);
+    }
+
+    private void setCameraPreferences(final ListPreference cameraPreference)
+    {
+        final int numberOfCameras = Camera.getNumberOfCameras();
+        final CharSequence[] entries = new CharSequence[numberOfCameras];
+        final CharSequence[] entryValues = new CharSequence[numberOfCameras];
+        final Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        for (int cameraIndex = 0; cameraIndex < numberOfCameras; cameraIndex++)
+        {
+            Camera.getCameraInfo(cameraIndex, cameraInfo);
+            String cameraFacing;
+            switch (cameraInfo.facing)
+            {
+                case Camera.CameraInfo.CAMERA_FACING_BACK:
+                    cameraFacing = getResources().getString(R.string.camera_back);
+                    break;
+                case Camera.CameraInfo.CAMERA_FACING_FRONT:
+                    cameraFacing = getResources().getString(R.string.camera_front);
+                    break;
+                default:
+                    cameraFacing = "unknown";
+            } // switch
+
+            entries[cameraIndex] = cameraFacing;
+            entryValues[cameraIndex] = String.valueOf(cameraIndex);
+        } //for
+
+        cameraPreference.setEntries(entries);
+        cameraPreference.setEntryValues(entryValues);
+        cameraPreference.setDefaultValue(0);
+        cameraPreference.setSummary(cameraPreference.getEntry());
+
+    } // setCameraPreferences(ListPreference)
+
+    private void setSizePreferences(final ListPreference sizePreference,
+                                    final ListPreference cameraPreference)
+    {
+        final String cameraPreferenceValue = cameraPreference.getValue();
+        final int cameraIndex;
+        if (cameraPreferenceValue != null)
+        {
+            cameraIndex = Integer.parseInt(cameraPreferenceValue);
+        } // if
+        else
+        {
+            cameraIndex = 0;
+        } // else
+        cameraDevice = getCameraInstance(cameraIndex);
+        final Camera.Parameters params = cameraDevice.getParameters();
+        releaseCamera();
+
+        final List<Camera.Size> supportedPreviewSizes = params.getSupportedPreviewSizes();
+        CharSequence[] entries = new CharSequence[supportedPreviewSizes.size()];
+        CharSequence[] entryValues = new CharSequence[supportedPreviewSizes.size()];
+        for (int previewSizeIndex = 0; previewSizeIndex < supportedPreviewSizes.size();
+             previewSizeIndex++)
+        {
+            Camera.Size supportedPreviewSize = supportedPreviewSizes.get(previewSizeIndex);
+            entries[previewSizeIndex] = supportedPreviewSize.width + "x"
+                    + supportedPreviewSize.height;
+            entryValues[previewSizeIndex] = String.valueOf(previewSizeIndex);
+        } // for
+
+        sizePreference.setEntries(entries);
+        sizePreference.setEntryValues(entryValues);
+        sizePreference.setDefaultValue(0);
+        sizePreference.setSummary(
+            String.format(getResources().getString(R.string.video_quality_summary),
+            sizePreference.getEntry())
+        );
+
+    } // setSizePreferenceData(ListPreference)
+
 
     private void releaseCamera(){
         if (cameraDevice != null){
@@ -83,4 +171,18 @@ public class UserSettingActivity extends PreferenceActivity {
         return c; // returns null if camera is unavailable
     }
 
+    /**
+     * Called when a shared preference is changed, added, or removed. This
+     * may be called even if a preference is set to its existing value.
+     * <p/>
+     * <p>This callback will be run on your main thread.
+     *
+     * @param sharedPreferences The {@link android.content.SharedPreferences} that received
+     *                          the change.
+     * @param key               The key of the preference that was changed, added, or
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
 }
